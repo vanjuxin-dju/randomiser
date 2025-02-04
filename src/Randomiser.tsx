05eaba99bd484a1dfe68aa9f938fc592bb6de71a
vanjuxin-dju <sokol.vdu@gmail.com>
2025-02-04 12:05:30 +0300

    added sound, improved logic of randomiser

--- src/Randomiser.tsx
+++ src/Randomiser.tsx
@@ -9,69 +9,34 @@ function Randomiser(): React.JSX.Element {
 		"some text 4",
 		"some text 5",
 		"some text 6",
-		"some text 1",
-		"some text 2",
-		"some text 3",
-		"some text 4",
-		"some text 5",
-		"some text 6",
-		"some text 1",
-		"some text 2",
-		"some text 3",
-		"some text 4",
-		"some text 5",
-		"some text 6",
-		"some text 1",
-		"some text 2",
-		"some text 3",
-		"some text 4",
-		"some text 5",
-		"some text 6",
-		"some text 1",
-		"some text 2",
-		"some text 3",
-		"some text 4",
-		"some text 5",
-		"some text 6",
-		"some text 1",
-		"some text 2",
-		"some text 3",
-		"some text 4",
-		"some text 5",
-		"some text 6",
-		"some text 1",
-		"some text 2",
-		"some text 3",
-		"some text 4",
-		"some text 5",
-		"some text 6",
-		"some text 1",
-		"some text 2",
-		"some text 3",
-		"some text 4",
-		"some text 5",
-		"some text 6",
+		"some text 7",
+		"some text 8",
+		"some text 9",
+		"some text 10",
+		"some text 11",
+		"some text 12",
 	];
 
 	let wheelTurning = false;
 
-	const angle = 360 / listOfOptions.length;
+	const sectorAngle = 360 / listOfOptions.length;
+	let currentAngle = 0;
 
 	let styles = `
 	.randomiser .sector::before {
-	  transform: rotate(${180 - angle}deg);
+	  transform: rotate(${180 - sectorAngle}deg);
 	}
 	.randomiser .sector .text {
-	  transform: rotate(-${angle / 2}deg);
-	  right: ${angle / 10}vmin;
-	  bottom: ${angle * 0.14}vmin;
+	  transform: rotate(-${sectorAngle / 2}deg);
+	  right: ${sectorAngle / 10}vmin;
+	  bottom: ${sectorAngle * 0.14}vmin;
 	}
 	`;
 
 	for (let i = 1; i < listOfOptions.length; i++) {
 		styles += `
 		.randomiser .sector:nth-child(${i + 1}) {
-          transform: rotate(${angle * i}deg);
+          transform: rotate(${sectorAngle * i}deg);
         }
 		`;
 	}
@@ -85,15 +50,19 @@ function Randomiser(): React.JSX.Element {
 			return;
 		}
 
-		let initialSpeed = 270 * Math.random();
+		const seed = 0.5 + Math.random() * 0.5;
+		const initialSpeed = 180 * seed;
 		let currentSpeedPerStep = initialSpeed / 20;
-		let currentElement = event.currentTarget;
-		let currentAngle = 0;
+		const currentElement = event.currentTarget;
+		const audioElement = currentElement.querySelector("audio");
+
 		wheelTurning = true;
+
+		audioElement.play();
 
 		setTimeout(function step() {
 			currentAngle += currentSpeedPerStep;
-			currentSpeedPerStep -= 0.01;
+			currentSpeedPerStep -= 0.02;
 
 			if (currentAngle >= 360) {
 				currentAngle -= 360;
@@ -105,6 +74,8 @@ function Randomiser(): React.JSX.Element {
 				setTimeout(step, 20);
 			} else {
 				wheelTurning = false;
+				audioElement.pause();
+				audioElement.currentTime = 0;
 			}
 			
 		}, 20);
@@ -113,6 +84,7 @@ function Randomiser(): React.JSX.Element {
 	return <div className="randomiser" onClick={turnTheWheel}>
 		{sectors}
 		<style>{styles}</style>
+		<audio src="/baraban_superigri.mp3"></audio>
 	</div>;
 }
 
